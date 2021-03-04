165ee593fb0c099ec43a3e45bd060db21c2610af
Victor Arjona <victorarjona2@gmail.com>
2021-03-03 23:17:47 -0800

    legendre plots with legend

--- spring-2021-math-543/hw4.py
+++ spring-2021-math-543/hw4.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def QR_MGS(A):
     m, n = A.shape
@@ -12,6 +13,7 @@ def QR_MGS(A):
         for jj in range(ii + 1, n):
             R[ii, jj] = np.dot(Q[:, ii].T, V[:, jj])
             V[:, jj] -= R[ii, jj]*Q[:, ii]
+
     return Q, R
 
 x = np.linspace(-128, 128, 257)/128
@@ -20,3 +22,14 @@ for ii in range(1, 4):
     A[:, ii] = x**ii
 
 Q, R = QR_MGS(A)
+
+lgnd = ["$x^{}$".format(ii) for ii in range(4)]
+plt_type = ["ro-", "b*-", "g-", "yx"]
+fig = plt.figure(figsize=(11, 7))
+ax = plt.gca()
+
+for ii in range(4):
+    ax.plot(Q[:, ii], label=lgnd[ii])
+ax.grid()
+ax.legend()
+plt.show()
