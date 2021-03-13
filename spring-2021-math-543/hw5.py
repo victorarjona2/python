7422c3062894b521172d67255d394616c19065f7
Victor Arjona <victorarjona2@gmail.com>
2021-03-12 21:52:04 -0800

    plots made

--- spring-2021-math-543/hw5.py
+++ spring-2021-math-543/hw5.py
@@ -1,11 +1,16 @@
 # Code provided by Stevie Dickerson, modified by Victor Arjona
 import numpy as np
 from numpy import linalg as la
+import matplotlib.pyplot as plt
 
-DEBUG = True
+DEBUG = False
 
-for n in range(3, 8):
-    m = 2**n
+fig, axs = plt.subplots(2, 3, figsize=(11, 7))
+axs = axs.flatten()
+lims = (-1.5, 1.5)
+
+for n in range(3, 9):
+    m = 2 ** n
     rho = np.array([])
     lam = np.array([])
     norms = np.array([])
@@ -16,10 +21,15 @@ for n in range(3, 8):
         rho = np.append(rho, max(abs(eigs_tmp)))
         lam = np.append(lam, eigs_tmp)
         # In the solution it's like this
-        #norms = np.append(la.norm(A, 2))
+        # norms = np.append(la.norm(A, 2))
         norms = np.append(norms, la.norm(A, 2))
 
     if DEBUG == True:
         print("Size of 100 Matrixes: {}x{}\n".format(m, m),
               "Size of rho: {}\n".format(rho.size),
-              "Size of lam: {}\n".format(lam.size))
+              "Size of lam: {}\n".format(lam.size),
+              "Size of norms: {}\n".format(norms.size))
+    real_lam = np.real(lam)
+    imag_lam = np.imag(lam)
+    axs[n - 3].plot(real_lam, imag_lam, 'o')
+    #axs[n - 3].set(xlim=lims, ylim=lims)
