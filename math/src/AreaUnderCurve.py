d365cc0b92fc955332205b11695ced3b2fc33031
victorarjona2 <victorarjona2@gmail.com>
2022-02-28 23:59:50 -0800

    start of reformatating
    moving files around and adding proper documentation to source code in math sub-repo

--- math/src/AreaUnderCurve.py
+++ math/src/AreaUnderCurve.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jun  1 10:23:55 2019
+Created on Sun Feb 27 22:36:19 2022
 
-@author: victor
+@author: victorarjona
 """
-# IMPORTS ------------------------------------------------------------------- #
+
 import numpy as np
 import matplotlib.pyplot as plt
-# IMPORTS ------------------------------------------------------------------- #
+
 '''
 DESCRIPTION
 
@@ -77,6 +77,8 @@ DESCRIPTION
 '''
 # HELPER FUNCTIONS ---------------------------------------------------------- #
 
+def X_Values(xlo, xhi, num_seg, integral_type):
+    pass
 
 def XInputs(x_lo, x_hi, no_rect, int_type='ms'):
     x_inp = np.linspace(x_lo, x_hi, no_rect + 1)
@@ -88,7 +90,6 @@ def XInputs(x_lo, x_hi, no_rect, int_type='ms'):
         return x_inp[0:-1]
     elif int_type == 'ts':
         return x_inp
-
 
 def GetArea(dx, fx, int_type='ms'):
     if int_type == 'ts':
@@ -119,7 +120,43 @@ def GetArea(dx, fx, int_type='ms'):
 #       1.1
 
 
-def ReimannApprox(f, x_lo, x_hi, tol=0.0001, int_type='ms', plot=False):
+def ReimannApprox(f, x_lo, x_hi, tol=4, int_type='ms', plot=False):
+    '''
+    Description
+        ReimannApprox makes an effort of approximating the integral (f) from
+        some point (x_lo) to another  (x_hi). The integral approximates up to
+        a tolerance (tol); this value defaults to 4 decimal points; if
+        specifying tolerance then it must be given as an integer, else it will
+        default back to 4 decimal points.
+        
+        There are many ways to approximate an integral (int_type). We can use
+        the right-most ("rs"), the left-most ("ls"), or middle point ("ms") to
+        generate a rectangle and add these together. A much better to do this
+        is using the trapezoid rule ("ts"). Refer to the link below for an
+        explanation of these different methods:
+            https://www.khanacademy.org/math/ap-calculus-ab/ab-integration-new/ab-6-2/a/riemann-sums-review
+        
+        By default, there are no plots generated (plot). When invoked
+        (plot=True), a graph of the approximation using 0 rectangles up to
+        the number of rectangles needed to reach the tolerance is generated.
+        
+        In the end we get the approximation of the integral (int_approx).
+        
+        Variables:
+            Required: 
+                f:          The function used to calculate the integral, AKA, 
+                            the integrand. It is assumed that the function is
+                            continuous between the lower and upper limit.
+                x_lo:       Lower limit.
+                x_hi:       Upper limit.
+            
+            Optional:
+                tol:        Tolerance. Up to how many decimal points should
+                            this approximation get to? E.G., if tol=3 then the
+                            tolerance is 10**(-3) (three decimal points).
+                int_type:   
+            
+    '''
     # APPROXIMATIONS CONTAINER. THE FIRST ELEMENT IS ONE SQUARE... THE
     # SECOND IS TWO... IN OTHER WORDS:
     # INDEX         NO. RECT
@@ -141,7 +178,7 @@ def ReimannApprox(f, x_lo, x_hi, tol=0.0001, int_type='ms', plot=False):
     # GENERATE THE Nth APPROXIMATION
     no_rects = 3
     print('GENERATING THE REST OF THE APPROXIMATIONS NOW:\n\n')
-    while (np.abs((int_approx[-1] - int_approx[-2])) > tol):
+    while (np.abs((int_approx[-1] - int_approx[-2])) > 10**(-1*tol):
         dx = (x_hi - x_lo)/float((no_rects))
         fx = f(XInputs(x_lo, x_hi, no_rects, int_type))
         approx = GetArea(dx, fx, int_type)
