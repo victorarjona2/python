0a388c3cfee3c9ce61bbbe1348e4390b56788c76
varjona <victorarjona2@gmail.com>
2024-07-31 01:26:04 -0700

    cleaneup

--- math/Roots.py
+++ math/Roots.py
@@ -11,7 +11,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
 class Roots():
     """
     The Roots class is a tool that provides a way to handle mathematical
@@ -30,7 +29,7 @@ class Roots():
         """
         The Roots class initialization starts by generating a Pandas DataFrame
         where the approximations, the output of the approximation,
-        the change in x, the change y f_x, and the actual Jacobian, assuming
+        the change in x, the change f_x, and the actual Jacobian, assuming
         it has been defined, are stored. 
         """
         dict_init = {"x": [],       # Approximations to x
@@ -69,47 +68,46 @@ class Roots():
 
         # If this is the first iteration, then populate values for first row!
         if len_iter_df == 0:
-            nu_row = [self.x_start,                         # First guess
-                      self.Function(self.x_start),          # First output
-                      np.NAN,                               # Change for xs
-                      np.NAN,                               # Change for ys
-                      self.Jacobian_Function(self.x_start)]  # Actual Jacobian
+            nu_row = [self.x_start,                             # First guess
+                      self.Function(self.x_start),              # Output of guess
+                      np.NAN,                                   # Change of guesses compared to previous
+                      np.NAN,                                   # Change of output compared to previous
+                      self.Jacobian_Function(self.x_start)]     # Jacobian of first guess
 
         # If this is the first or later iteration, then...
         elif len_iter_df > 0:
             # Prep variables for use...
             pre_row = self.iter_df.iloc[-1]
 
-            pre_x = pre_row["x"]
-            pre_f_x = pre_row["f_x"]
-            pre_Jf_x = pre_row["Jf_x"]
-
             # Generate new values for new row...
             try:
-                pre_inv_Jf_x = np.linalg.inv(pre_Jf_x)
-                nu_x = pre_x - pre_inv_Jf_x@(pre_f_x - self.des_y)
+                pre_inv_Jf_x = np.linalg.inv(pre_row["Jf_x"])
             except:
-                pre_inv_Jf_x = 1/pre_Jf_x
-                nu_x = pre_x - pre_inv_Jf_x*(pre_f_x - self.des_y)
+                pre_inv_Jf_x = 1/pre_row["Jf_x"]
 
-            # Make new f_x
+            # Make new x, f_x
+            nu_x = pre_row["x"] - pre_inv_Jf_x * (pre_row["fx"] - self.des_y)
             nu_f_x = self.Function(nu_x)
 
             # Make new row
             nu_row = [nu_x,
                       nu_f_x,
-                      nu_x - pre_x,
-                      nu_f_x - pre_f_x,
+                      nu_x - pre_row["x"],
+                      nu_f_x - pre_row["fx"],
                       self.Jacobian_Function(nu_x)]
 
         nu_row = np.asarray(nu_row, dtype=object)
+
         if self.debug:
             print(nu_row)
 
         # Add row to DataFrame.
         self.iter_df.loc[len_iter_df] = nu_row
 
-    def Newton_Method(self, x_start=0, des_y=0, eps=np.finfo(float).eps,
+    def Newton_Method(self,
+                      x_start=0,
+                      des_y=0,
+                      eps=np.finfo(float).eps,
                       debug=False):
         '''
         '''
@@ -129,9 +127,10 @@ class Roots():
         except:
             curr_dx = np.NAN
 
-        while curr_dx == np.NAN or np.linalg.norm(curr_dx) > self.eps:
+        self.Iterate()
+
+        while np.linalg.norm(self.iter_df.iloc[-1]["dx"]) > self.eps:
             self.Iterate()
-            curr_dx = self.iter_df.iloc[-1]["dx"]
 
         if self.debug:
             print(self.iter_df)
@@ -166,8 +165,7 @@ def Demo_Funcs2():
         a = np.power(x[1], x[0]**2 + 7*x[0] + 12)
         b = np.power(x[1], x[0]**2 + 7*x[0] + 11)
 
-        row0_Jf_x0_x1 = [(2*x[0] + 7)*a*np.log(x[1]),
-                         (x[0]**2 + 7*x[0] + 12)*b]
+        row0_Jf_x0_x1 = [(2*x[0] + 7)*a*np.log(x[1]), (x[0]**2 + 7*x[0] + 12)*b]
         row1_Jf_x0_x1 = [1, 1]
 
         Jf_x = np.array([row0_Jf_x0_x1, row1_Jf_x0_x1])
@@ -186,6 +184,8 @@ if __name__ == "__main__":
     quad_rooty.Define_Jacobian_Function(Jf)
     quad_rooty.Newton_Method(eps=0.0000001,
                              x_start=-999)
+    plt.plot(quad_rooty.iter_df.loc[:, "x":"f_x"])
+    plt.show()
 
     print("Starting non-linear system of equations demo 1!")
     non_linear_sys_eq_rooty = Roots()
@@ -206,7 +206,7 @@ if __name__ == "__main__":
     non_linear_sys_eq_rooty2.Define_Function(f)
     non_linear_sys_eq_rooty2.Define_Jacobian_Function(Jf)
     dest_y = np.array([1, 6])
-    start_x = np.array([-10, 5])
+    start_x = np.array([-8, 5])
     non_linear_sys_eq_rooty2.Newton_Method(x_start=start_x,
                                            des_y=dest_y,
                                            eps=0.0000001)
